76e5a515fb3e2dc63e0d410ed24723c097ab6d1e
Ainsel <15658969+myAinsel@users.noreply.github.com>
2023-10-20 15:10:24 -0500

    Provide examples of UCI

--- humanplayer.py
+++ humanplayer.py
@@ -5,6 +5,8 @@ from player import Player
 class HumanPlayer(Player):
     """Represents a human or other sapient player."""
     def MakeMove(self, board):
+        print("UCI Format: {starting square}{x if capturing}{ending square}{piece symbol if promoting}")
+        print("e.g.: e2e4, e7e8q")
         move = input("Please type a move in UCI format")
         while move not in list(board.legal_moves):
             print("Move is not legal!")
