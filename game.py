bdf21a5b9b019449b2507ba817adae753fa0c6c5
Morgan <15658969+myAinsel@users.noreply.github.com>
2023-10-24 16:46:24 -0500

    Updated export methods

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
 import chess
 import chess.pgn
-import datetime
 from player import Player
 
+import datetime
+import os
 
 class Game:
     """This class represents an arbitrary chess game"""
@@ -20,6 +21,9 @@ class Game:
             chess.WHITE: {"kingside": True, "queenside": True}, 
             chess.BLACK: {"kingside": True, "queenside": True}
             }
+        
+        self.date = datetime.date.isoformat(datetime.date.today())
+        self.endtime = None
         self.victor = None
 
     def __repr__(self):
@@ -50,17 +54,17 @@ class Game:
         
         self.has_en_passant[self.turn] = self.board.has_legal_en_passant
         self.can_castle[self.turn] = self.board.has_castling_rights(self.turn)
-        self.turn = self.board.turn #  Run this AFTER any attribute updates
+        self.turn = self.board.turn  # Run this AFTER any attribute updates
 
-        if self.board.is_checkmate():
-            self.game_over()
         return True
 
     def game_over(self):
         """Declares a victor for the Game and updates the Players' scores.
         Assumes Game has been concluded.
         """
+        self.endtime = datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S')
         self.victor = self.board.outcome().winner
+
         if self.victor == chess.WHITE:
             self.player_white.score["Wins"] += 1
             self.player_black.score["Losses"] += 1
@@ -87,6 +91,9 @@ class Game:
                 print("Black's move")
                 self.choose_move(self.player_black.MakeMove(self.board))
             turn += 1
+        if self.board.is_game_over():
+            self.game_over()
+
         if self.victor == True:
            print("Game over! White wins!") 
         elif self.victor == False: 
@@ -94,21 +101,50 @@ class Game:
         else:
             print("Game over! Draw!")
     
-    def export_game(self, filename=None):
-        """Export the Game to a .pgn file"""
-        time_now = datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S')
-        date_now = datetime.date.isoformat(datetime.date.today())
-        if filename is None:
-            filename = "blobfish-" + time_now + ".pgn"
+    def game_to_pgn(self):
+        """Returns a chess.pgn.Game object with headers which can be exported
+        or added to a Scoreboard.
+        NOTE: Currently, Games are defined separately from each other; that is,
+        the Round header is not used, even if two Games are meant to continue
+        each other.
+        """
+        if self.endtime is None:  # If the Game is over or not
+            game_time = datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S')
+        else:
+            game_time = self.endtime
         match = chess.pgn.Game.from_board(self.board)
         
-        match.headers["Event"] = "Blobfish Match " + time_now
+        match.headers["Event"] = "Blobfish Match " + game_time
         match.headers["Site"] = "Blobfish Engine"
-        match.headers["Date"] = date_now
-        match.headers["Round"] = 1 #  TODO
+        match.headers["Date"] = self.date
+        match.headers["Round"] = 1  # TODO
         match.headers["White"] = self.player_white.name
         match.headers["Black"] = self.player_black.name
-
+        
+        if self.victor == chess.WHITE:
+            match.headers["Result"] = "1-0"
+        elif self.victor == chess.BLACK:
+            match.headers["Result"] = "0-1"
+        else:
+            # Handle None victor cases (draw vs in-progress) separately
+            if self.board.is_game_over():
+                match.headers["Result"] = "1/2-1/2"
+            else:
+                match.headers["Result"] = "*"
+        return match
+        
+    def export_game(self, filename=None):
+        """Export the Game to a .pgn file. Optionally takes a filename to 
+        use. If provided, filename should end in ".pgn".
+        """
+        match = self.game_to_pgn()
+        if self.endtime is None:  # If the Game is over or not
+            game_time = datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S')
+        else:
+            game_time = self.endtime
+        if filename is None:
+            filename = "blobfish-" + game_time + ".pgn"
+        
         with open(filename, "w") as file:
             file.write(str(match))
 
@@ -116,11 +152,50 @@ class Scoreboard:
     """This class represents a history of played games"""
     def __init__(self):
         self.scoreboard = {chess.WHITE: 0, chess.BLACK: 0, None: 0}
-        self.record = [] # A list of past Games, which can be reviewed later
+        self.record = []  # A list of past Games, which can be reviewed later
 
     def update(self, game: Game):
         """Increments the scoreboard and updates the record.
         Assumes Game has been concluded.
         """
         self.scoreboard[game.victor] += 1
-        self.record.append(game)
+        pgn_game = game.game_to_pgn()
+        self.record.append(pgn_game)
+
+    def export_scoreboard(self):
+        """Exports the Scoreboard to a folder in the current working directory,
+        containing a .pgn file for every game in the record.
+        """
+        time_now = datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S')
+        dir_name = "blobfish-record-" + time_now
+        os.mkdir(dir_name)
+
+        for game in self.record:
+            filename = str(game.headers["Event"]) +".pgn"
+            filename = os.path.join(dir_name, filename)
+            with open(filename, "a") as file:
+                file.write(str(game))
+        
+        # Write the actual "scoreboard" to a file
+        with open(dir_name + "/record.txt", "w") as f:
+            f.write(str(self.scoreboard))
+
+    def import_scoreboard(self, dir, overwrite=False):
+        """Reads a given directory for .pgn files and adds them to the
+        Scoreboard. If the overwrite flag is passed, the existing data is 
+        overwritten. 
+        """
+        if overwrite:
+            self.scoreboard = {chess.WHITE: 0, chess.BLACK: 0, None: 0}
+            self.record = []
+        
+        if not os.path.isdir(dir):
+            raise FileNotFoundError
+        # Get a list of only .pgn files in dir
+        files = []
+        for f in os.listdir(dir):
+            if os.path.isfile(f) and os.path.splitext(f)[1] == ".pgn":
+                files.append(f)
+        for f in files:
+            file = open(os.path.join(dir,f), "r")
+            match = chess.pgn.read_game(file)
